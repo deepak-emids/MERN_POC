e76da5d2d5e52173d95f8f02b326f15285fd1088
deepak-emids <deepak.kumbhar@quovantis.com>
2022-04-05 14:32:11 +0530

    [deepak]Added:test for signin component if it calls next function onClick button

---
+++ src/components/signin/signin.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Signin from "./Signin";
+import * as foo from "./Signin";
+
+import userEvent from "@testing-library/user-event";
+
+foo.next = jest.fn().mockResolvedValue("");
+// jest.mock("./Signin", () => ({ next: jest.fn() }));
+
+// jest.mock("./Signin", () => ({
+//   ...jest.requireActual("./Signin"),
+//   next: jest.fn(),
+// }));
+
+it("when clicked on login button should call the next function", () => {
+  render(<Signin />);
+
+  const buttonElement = screen.getByRole("button", { name: /LOGIN/i });
+  userEvent.click(buttonElement);
+  expect(foo.next).toHaveBeenCalled();
+});
