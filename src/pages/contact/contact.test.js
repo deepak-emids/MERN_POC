658b6c6976eb6c62c4b136717e919c1ead66bc43
deepak-emids <deepak.kumbhar@quovantis.com>
2022-04-05 14:24:21 +0530

    [deepak]Added:test for contact page to test if it renders image

--- src/pages/contact/contact.test.js
+++ src/pages/contact/contact.test.js
@@ -13,3 +13,9 @@ it("when render the contact page should contaion tagline", () => {
   const img = screen.getByTestId("table1");
   expect(img).toBeInTheDocument();
 });
+
+it("when render the contact page should contain vec image 2", () => {
+  render(<Contact />);
+  const img = screen.getByTestId("contact1");
+  expect(img).toBeInTheDocument();
+});
