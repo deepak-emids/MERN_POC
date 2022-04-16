940c3cc3c1cdf7d3679599c6aabfee594f9c7c94
deepak-emids <deepak.kumbhar@quovantis.com>
2022-04-16 11:30:06 +0530

    [deepak]added:rename employee controller

---
+++ src/controllers/EmployeeController.ts
@@ -0,0 +1,97 @@
+import { Request, Response, NextFunction } from 'express';
+import EmployeeDetailsService from '../services/EmployeeDetailsService';
+import logger from '../config/logger';
+import ResponseModel from '../models/Response.model';
+
+class EmployeeDetailsController {
+  private employeeDetailsService;
+  constructor(employeeDetailsService?: EmployeeDetailsService) {
+    this.employeeDetailsService = employeeDetailsService
+      ? employeeDetailsService
+      : new EmployeeDetailsService();
+  }
+  public addEmployeeDetails = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> => {
+    try {
+      const data: ResponseModel = await this.employeeDetailsService.addEmployee(
+        req.body
+      );
+
+      res.status(data.status).send(data);
+    } catch (error) {
+      logger.logger.error(error);
+      next(error);
+    }
+  };
+
+  public getAllEmployeeDetails = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> => {
+    try {
+      const data: ResponseModel =
+        await this.employeeDetailsService.getAllEmployee();
+      res.status(data.status).send(data);
+    } catch (error) {
+      logger.logger.error(error);
+      next(error);
+    }
+  };
+
+  public getEmployeeDetails = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> => {
+    try {
+      const data: ResponseModel =
+        await this.employeeDetailsService.getEmployeeDetails(req.params.id);
+      res.status(data.status).send(data);
+    } catch (error) {
+      logger.logger.error(error);
+
+      next(error);
+    }
+  };
+
+  public deleteEmployeeDetails = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> => {
+    try {
+      const data: ResponseModel =
+        await this.employeeDetailsService.deleteEmployeeDetails(req.params.id);
+      res.status(data.status).send(data);
+    } catch (error) {
+      logger.logger.error(error);
+
+      next(error);
+    }
+  };
+
+  public updateEmployeeDetails = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> => {
+    try {
+      const data: ResponseModel =
+        await this.employeeDetailsService.updateEmployeeDetails(
+          req.params.id,
+          req.body
+        );
+      res.status(data.status).send(data);
+    } catch (error) {
+      logger.logger.error(error);
+
+      next(error);
+    }
+  };
+}
+
+export default EmployeeDetailsController;
