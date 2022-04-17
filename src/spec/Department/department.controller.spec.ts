a3be719a8f874f5f122d95b251932c0c09a2c966
deepak-emids <deepak.kumbhar@quovantis.com>
2022-04-17 21:43:12 +0530

    [deepak]added:unit test of controller  module for addDepartment, to be defined

--- src/spec/Department/department.controller.spec.ts
+++ src/spec/Department/department.controller.spec.ts
@@ -23,4 +23,8 @@ describe('testing Department controller', () => {
   it('when given a controller methods it should return type of method to be function', async () => {
     expect(typeof departmentController.addDepartment).toBe('function');
   });
+
+  it('when given a controller method it should be defined', async () => {
+    expect(departmentController.addDepartment).toBeDefined();
+  });
 });
