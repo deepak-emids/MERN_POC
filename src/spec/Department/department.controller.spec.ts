8b0040117831d8b1441e904d97c023414dd5f3ca
deepak-emids <deepak.kumbhar@quovantis.com>
2022-04-17 21:35:53 +0530

    [deepak]added:unit test of controller  module for addDepartment, to be typeof funtion

---
+++ src/spec/Department/department.controller.spec.ts
@@ -0,0 +1,25 @@
+import httpMock from 'node-mocks-http';
+import DepartmentService from '../../services/DepartmentService';
+
+import DepartmentController from '../../controllers/DepartmentController';
+
+let req: any, res: any, next: any;
+
+beforeEach(() => {
+  req = httpMock.createRequest({
+    params: {
+      id: 5
+    }
+  });
+  res = httpMock.createResponse({});
+  next = () => {};
+});
+
+let departmentService = new DepartmentService();
+
+let departmentController = new DepartmentController(departmentService);
+
+describe('testing Department controller', () => {
+  it('when given a controller methods it should return type of method to be function', async () => {
+    expect(typeof departmentController.addDepartment).toBe('function');
+  });
