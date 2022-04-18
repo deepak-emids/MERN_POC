d6e10ee8c93fd0839c899c8ab7f265de943e5f72
deepak-emids <deepak.kumbhar@quovantis.com>
2022-04-18 09:21:54 +0530

    [deepak]added:unit test of controller module for updatedDepartmenttoBeFunction

--- src/spec/Department/department.controller.spec.ts
+++ src/spec/Department/department.controller.spec.ts
@@ -1,98 +1,104 @@
-// import httpMock from 'node-mocks-http';
-// import DepartmentService from '../../services/DepartmentService';
+import httpMock from 'node-mocks-http';
+import DepartmentService from '../../services/DepartmentService';
 
-// import DepartmentController from '../../controllers/DepartmentController';
+import DepartmentController from '../../controllers/DepartmentController';
 
-// let req: any, res: any, next: any;
+let req: any, res: any, next: any;
 
-// beforeEach(() => {
-//   req = httpMock.createRequest({
-//     params: {
-//       id: 9
-//     }
-//   });
-//   res = httpMock.createResponse({});
-//   next = () => {};
-// });
+const newDepartment = { departmentName: 'newDepartment' };
 
-// let departmentService = new DepartmentService();
+beforeEach(() => {
+  req = httpMock.createRequest({
+    params: {
+      id: 9
+    },
+    body: newDepartment
+  });
+  res = httpMock.createResponse({});
+  next = () => {};
+});
 
-// let departmentController = new DepartmentController(departmentService);
+let departmentService = new DepartmentService();
 
-// describe('testing Department controller', () => {
-//   it('when given a controller methods it should return type of method to be function', async () => {
-//     expect(typeof departmentController.addDepartment).toBe('function');
-//   });
+let departmentController = new DepartmentController(departmentService);
 
-//   it('when given a controller method it should be defined', async () => {
-//     expect(departmentController.addDepartment).toBeDefined();
-//   });
+describe('testing Department controller', () => {
+  it('when given a controller methods it should return type of method to be function', async () => {
+    expect(typeof departmentController.addDepartment).toBe('function');
+  });
 
-//   it('when a give controller method is called it should call corresponding service method', async () => {
-//     departmentService.addDepartment = jest.fn();
-//     await departmentController.addDepartment(req, res, next);
+  it('when given a controller method it should be defined', async () => {
+    expect(departmentController.addDepartment).toBeDefined();
+  });
 
-//     expect(departmentService.addDepartment).toHaveBeenCalled();
-//   });
+  it('when a give controller method is called it should call corresponding service method', async () => {
+    departmentService.addDepartment = jest.fn();
+    await departmentController.addDepartment(req, res, next);
 
-//   it('when given a controller methods it should return type of method to be function', async () => {
-//     expect(typeof departmentController.getAllDepartment).toBe('function');
-//   });
+    expect(departmentService.addDepartment).toHaveBeenCalled();
+  });
 
-//   it('when given a controller method it should be defined', async () => {
-//     expect(departmentController.getAllDepartment).toBeDefined();
-//   });
+  it('when given a controller methods it should return type of method to be function', async () => {
+    expect(typeof departmentController.getAllDepartment).toBe('function');
+  });
 
-//   it('when a give controller method is called it should call corresponding service method', async () => {
-//     departmentService.getAllDepartment = jest.fn();
-//     await departmentController.getAllDepartment(req, res, next);
+  it('when given a controller method it should be defined', async () => {
+    expect(departmentController.getAllDepartment).toBeDefined();
+  });
 
-//     expect(departmentService.getAllDepartment).toHaveBeenCalled();
-//   });
+  it('when a give controller method is called it should call corresponding service method', async () => {
+    departmentService.getAllDepartment = jest.fn();
+    await departmentController.getAllDepartment(req, res, next);
 
-//   it('when given a controller method getDepartment it should return type of method to be function', async () => {
-//     expect(typeof departmentController.getDepartment).toBe('function');
-//   });
+    expect(departmentService.getAllDepartment).toHaveBeenCalled();
+  });
 
-//   it('when given a controller method getDepartment it should be defined', async () => {
-//     expect(departmentController.getDepartment).toBeDefined();
-//   });
+  it('when given a controller method getDepartment it should return type of method to be function', async () => {
+    expect(typeof departmentController.getDepartment).toBe('function');
+  });
 
-//   it('when a give controller  method getDepartment is called it should call corresponding service method', async () => {
-//     departmentService.getDepartment = jest.fn();
-//     await departmentController.getDepartment(req, res, next);
+  it('when given a controller method getDepartment it should be defined', async () => {
+    expect(departmentController.getDepartment).toBeDefined();
+  });
 
-//     expect(departmentService.getDepartment).toHaveBeenCalled();
-//   });
+  it('when a give controller  method getDepartment is called it should call corresponding service method', async () => {
+    departmentService.getDepartment = jest.fn();
+    await departmentController.getDepartment(req, res, next);
 
-//   it('when a give controller getDepartment method is called it should call corresponding service method with parameters', async () => {
-//     departmentService.getDepartment = jest.fn();
-//     await departmentController.getDepartment(req, res, next);
+    expect(departmentService.getDepartment).toHaveBeenCalled();
+  });
 
-//     expect(departmentService.getDepartment).toBeCalledWith(req.params.id);
-//   });
+  it('when a give controller getDepartment method is called it should call corresponding service method with parameters', async () => {
+    departmentService.getDepartment = jest.fn();
+    await departmentController.getDepartment(req, res, next);
 
-//   it('when given a controller methods it should return type of method to be function', async () => {
-//     departmentController.getDepartment = jest.fn();
+    expect(departmentService.getDepartment).toBeCalledWith(req.params.id);
+  });
 
-//     expect(typeof departmentController.deleteDepartment).toBe('function');
-//   });
+  it('when given a controller methods it should return type of method to be function', async () => {
+    departmentController.getDepartment = jest.fn();
+    expect(typeof departmentController.deleteDepartment).toBe('function');
+  });
 
-//   it('when given a controller method it should be defined', async () => {
-//     expect(departmentController.deleteDepartment).toBeDefined();
-//   });
+  it('when given a controller method it should be defined', async () => {
+    expect(departmentController.deleteDepartment).toBeDefined();
+  });
 
-//   it('when a give controller method is called it should call corresponding service method', async () => {
-//     departmentService.deleteDepartment = jest.fn();
-//     await departmentController.deleteDepartment(req, res, next);
+  it('when a give controller method is called it should call corresponding service method', async () => {
+    departmentService.deleteDepartment = jest.fn();
+    await departmentController.deleteDepartment(req, res, next);
 
-//     expect(departmentService.deleteDepartment).toHaveBeenCalled();
-//   });
+    expect(departmentService.deleteDepartment).toHaveBeenCalled();
+  });
 
-//   it('when a give controller method is called it should call corresponding service method with parameters', async () => {
-//     departmentService.deleteDepartment = jest.fn();
-//     await departmentController.deleteDepartment(req, res, next);
+  it('when a give controller method is called it should call corresponding service method with parameters', async () => {
+    departmentService.deleteDepartment = jest.fn();
+    await departmentController.deleteDepartment(req, res, next);
 
-//     expect(departmentService.deleteDepartment).toBeCalledWith(req.params.id);
-//   });
-// });
+    expect(departmentService.deleteDepartment).toBeCalledWith(req.params.id);
+  });
+
+  it('when given a controller method updateDepartment it should return type of method to be function', async () => {
+    expect(typeof departmentController.updateDepartment).toBe('function');
+  });
+});
