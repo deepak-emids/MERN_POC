107d99dba8c3452c9852e393323c1247915f13ed
deepak-emids <deepak.kumbhar@quovantis.com>
2022-04-18 09:40:40 +0530

    [deepak]added:unit test of service module for updatedDepartment toBeFunction

--- src/spec/Department/department.service.spec.ts
+++ src/spec/Department/department.service.spec.ts
@@ -1,100 +1,108 @@
-// import httpMock from 'node-mocks-http';
-// import DepartmentRepository from '../../repository/DepartmentRepository';
+import httpMock from 'node-mocks-http';
+import DepartmentRepository from '../../repository/DepartmentRepository';
 
-// import DepartmentService from '../../services/DepartmentService';
+import DepartmentService from '../../services/DepartmentService';
 
-// let req: any, res: any, next: any;
-// const newDepartment = { departmentName: 'testDepartment' };
-// const id: number = 9;
+let req: any, res: any, next: any;
 
-// beforeEach(() => {
-//   req = httpMock.createRequest({
-//     params: {
-//       id: 9
-//     }
-//   });
-//   res = httpMock.createResponse({});
-//   next = () => {};
-// });
+const newDepartment = { departmentName: 'testDepartment' };
 
-// let departmentRepository = new DepartmentRepository();
+const id: number = 9;
 
-// let departmentService = new DepartmentService(departmentRepository);
+beforeEach(() => {
+  req = httpMock.createRequest({
+    params: {
+      id: 9
+    }
+  });
+  res = httpMock.createResponse({});
+  next = () => {};
+});
 
-// describe('unit tests for employee service module', () => {
-//   it('when given a service method addDepartment it should return type of method to be function', async () => {
-//     expect(typeof departmentService.addDepartment).toBe('function');
-//   });
+let departmentRepository = new DepartmentRepository();
 
-//   it('when given a service method addDepartment it should be defined', async () => {
-//     expect(departmentService.addDepartment).toBeDefined();
-//   });
+let departmentService = new DepartmentService(departmentRepository);
 
-//   it('when a give service method addDepartment is called it should call corresponding service method', async () => {
-//     departmentRepository.add = jest.fn();
-//     await departmentService.addDepartment(newDepartment);
+describe('unit tests for employee service module', () => {
+  it('when given a service method addDepartment it should return type of method to be function', async () => {
+    expect(typeof departmentService.addDepartment).toBe('function');
+  });
 
-//     expect(departmentRepository.add).toHaveBeenCalled();
-//   });
+  it('when given a service method addDepartment it should be defined', async () => {
+    expect(departmentService.addDepartment).toBeDefined();
+  });
 
-//   it('when given a service method getAllDepartment it should return type of method to be function', async () => {
-//     expect(typeof departmentService.getAllDepartment).toBe('function');
-//   });
+  it('when a give service method addDepartment is called it should call corresponding service method', async () => {
+    departmentRepository.add = jest.fn();
+    await departmentService.addDepartment(newDepartment);
 
-//   it('when given a service method getAllDepartment it should be defined', async () => {
-//     expect(departmentService.getAllDepartment).toBeDefined();
-//   });
+    expect(departmentRepository.add).toHaveBeenCalled();
+  });
 
-//   it('when a give service method getAllDepartmentis called it should call corresponding service method', async () => {
-//     departmentRepository.getAll = jest.fn();
-//     await departmentService.getAllDepartment();
+  it('when given a service method getAllDepartment it should return type of method to be function', async () => {
+    expect(typeof departmentService.getAllDepartment).toBe('function');
+  });
 
-//     expect(departmentRepository.getAll).toHaveBeenCalled();
-//   });
+  it('when given a service method getAllDepartment it should be defined', async () => {
+    expect(departmentService.getAllDepartment).toBeDefined();
+  });
 
-//   it('when given a service method getDepartment it should return type of method to be function', async () => {
-//     expect(typeof departmentService.getDepartment).toBe('function');
-//   });
+  it('when a give service method getAllDepartmentis called it should call corresponding service method', async () => {
+    departmentRepository.getAll = jest.fn();
+    await departmentService.getAllDepartment();
 
-//   it('when given a service method getDepartment it should be defined', async () => {
-//     expect(departmentService.getDepartment).toBeDefined();
-//   });
+    expect(departmentRepository.getAll).toHaveBeenCalled();
+  });
 
-//   it('when a give service method getDepartment is called it should call corresponding service method', async () => {
-//     departmentRepository.get = jest.fn();
-//     await departmentService.getDepartment(id);
+  it('when given a service method getDepartment it should return type of method to be function', async () => {
+    expect(typeof departmentService.getDepartment).toBe('function');
+  });
 
-//     expect(departmentRepository.get).toHaveBeenCalled();
-//   });
+  it('when given a service method getDepartment it should be defined', async () => {
+    expect(departmentService.getDepartment).toBeDefined();
+  });
 
-//   it('when a give service method getDepartment is called it should call corresponding service method with parameters', async () => {
-//     departmentRepository.get = jest.fn();
-//     await departmentService.getDepartment(id);
+  it('when a give service method getDepartment is called it should call corresponding service method', async () => {
+    departmentRepository.get = jest.fn();
+    await departmentService.getDepartment(id);
 
-//     expect(departmentRepository.get).toBeCalledWith({ id: id });
-//   });
+    expect(departmentRepository.get).toHaveBeenCalled();
+  });
 
-//   it('when given a service method getDepartment it should return type of method to be function', async () => {
-//     departmentService.getDepartment = jest.fn();
+  it('when a give service method getDepartment is called it should call corresponding service method with parameters', async () => {
+    departmentRepository.get = jest.fn();
+    await departmentService.getDepartment(id);
 
-//     expect(typeof departmentService.deleteDepartment).toBe('function');
-//   });
+    expect(departmentRepository.get).toBeCalledWith({ id: id });
+  });
 
-//   it('when given a service method deleteDepartment it should be defined', async () => {
-//     expect(departmentService.deleteDepartment).toBeDefined();
-//   });
+  it('when given a service method deleteDepartment it should return type of method to be function', async () => {
+    departmentService.deleteDepartment = jest.fn();
 
-//   it('when given a service method deleteDepartment is called it should call corresponding service method', async () => {
-//     departmentRepository.delete = jest.fn();
-//     await departmentService.deleteDepartment(id);
+    expect(typeof departmentService.deleteDepartment).toBe('function');
+  });
 
-//     expect(departmentRepository.delete).toHaveBeenCalled();
-//   });
+  it('when given a service method deleteDepartment it should be defined', async () => {
+    expect(departmentService.deleteDepartment).toBeDefined();
+  });
 
-//   it('when given a service method deleteDepartment is called it should call corresponding service method with parameters', async () => {
-//     departmentRepository.delete = jest.fn();
-//     await departmentService.deleteDepartment(id);
+  it('when given a service method deleteDepartment is called it should call corresponding service method', async () => {
+    departmentRepository.delete = jest.fn();
+    await departmentService.deleteDepartment(id);
 
-//     expect(departmentRepository.delete).toBeCalledWith(req.params.id);
-//   });
-// });
+    expect(departmentRepository.delete).toHaveBeenCalled();
+  });
+
+  it('when given a service method deleteDepartment is called it should call corresponding service method with parameters', async () => {
+    departmentRepository.delete = jest.fn();
+    await departmentService.deleteDepartment(id);
+
+    expect(departmentRepository.delete).toBeCalledWith(req.params.id);
+  });
+
+  it('when given a service method updateDepartment it should return type of method to be function', async () => {
+    departmentService.updateDepartment = jest.fn();
+
+    expect(typeof departmentService.updateDepartment).toBe('function');
+  });
+});
