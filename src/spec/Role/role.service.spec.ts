6be45df771f7a24e0d0db4fca689d87b44fe07a5
deepak-emids <deepak.kumbhar@quovantis.com>
2022-04-18 07:57:31 +0530

    [deepak]added:unit test of service module for addRole, toBeDefined

--- src/spec/Role/role.service.spec.ts
+++ src/spec/Role/role.service.spec.ts
@@ -23,4 +23,8 @@ describe('testing role controller', () => {
   it('when given a controller methods it should return type of method to be function', async () => {
     expect(typeof roleController.addRole).toBe('function');
   });
+
+  it('when given a controller method it should be defined', async () => {
+    expect(roleController.addRole).toBeDefined();
+  });
 });
